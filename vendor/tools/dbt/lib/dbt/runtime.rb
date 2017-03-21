fe3963c74d8f8c0837750ad04437730544c919af
Peter Donald <peter@realityforge.org>
2017-03-21 15:37:08 +1100

    Braid: Update mirror 'vendor/tools/dbt' to '609e33b'

--- vendor/tools/dbt/lib/dbt/runtime.rb
+++ vendor/tools/dbt/lib/dbt/runtime.rb
@@ -65,6 +65,12 @@ TXT
       end
     end
 
+    def query_in_control_database(database, sql)
+      init_control_database(database.key) do
+        return db.query(sql)
+      end
+    end
+
     def execute(database, sql)
       init_database(database.key) do
         db.execute(sql)
@@ -710,7 +716,7 @@ TXT
 
     def generate_standard_sequence_import_sql(sequence_name)
       sql = "DECLARE @Next VARCHAR(50);\n"
-      sql += "SELECT @Next = CAST(current_value AS BIGINT) + 1 FROM [@@TARGET@@].sys.sequences WHERE object_id = OBJECT_ID('[@@TARGET@@].#{sequence_name}');\n"
+      sql += "SELECT @Next = CAST(current_value AS BIGINT) + 1 FROM [@@SOURCE@@].sys.sequences WHERE object_id = OBJECT_ID('[@@SOURCE@@].#{sequence_name}');\n"
       sql += "SET @Next = COALESCE(@Next,'1');"
       sql += "EXEC('USE [@@TARGET@@]; ALTER SEQUENCE #{sequence_name} RESTART WITH ' + @Next );"
       sql
